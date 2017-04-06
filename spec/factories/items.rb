5f0f0da080d1f5695502e48f1cf0f9179f806b30
Nate <ndake11@gmail.com>
2017-04-05 20:26:40 -0400

    User, List, Item, model and serializers created

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :item do
+    list nil
+    description "MyString"
+    completed false
+  end
+end
---
+++ spec/factories/lists.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :list do
+    name "MyString"
+    user nil
+    permission "MyString"
+  end
+end
