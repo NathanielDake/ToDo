842c04b38edad7327a4fed738e5e0b17026add4c
Nate <ndake11@gmail.com>
2017-04-15 12:53:17 -0400

    List and item updating completed

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -4,15 +4,32 @@ class Api::ItemsController < ApiController
   before_action :authenticated?
 
   def create
-    item = Item.new(params.require(:item).permit(:description))
+    list = List.find(params[:list_id])
+    item = list.items.build(item_params)
 
     if item.save
       render json: item
     else
-      render json: { errors: item.errors.full_messages, status: :unprocessable_entity}
+      render json: { errors: item.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
+  def update
+    list = List.find(params[:list_id])
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      render json: item, status: 200
+    else
+      render json: {}, status: 422
+    end
+
+
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:description)
+  end
 end
 
 
--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -3,7 +3,8 @@ class Api::ListsController < ApiController
   before_action :authenticated?
 
   def create
-    list = List.new(params.require(:list).permit(:name, :permission))
+    user = User.find(params[:user_id])
+    list = user.lists.build(list_params)
 
     if list.save
       render json: list
@@ -12,6 +13,16 @@ class Api::ListsController < ApiController
     end
   end
 
+  def update
+    list = List.find(params[:id])
+    if list.update(list_params)
+      render json: list
+    else
+      render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+
   def destroy
     list = List.find(params[:id])
     list.destroy
@@ -19,5 +30,11 @@ class Api::ListsController < ApiController
     render json: {}, status: :no_content
   rescue ActiveRecord::RecordNotFound
     render :json => {}, :status => :not_found
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :permission)
   end
 end
